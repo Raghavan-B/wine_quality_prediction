de38cd326150a54d63868db89b6f6ebd826ebc64
Raghavan-B <nightmareraghs17@gmail.com>
2024-10-27 23:34:07 +0530

    Model training completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.wine_quality_prediction import logger
 from src.wine_quality_prediction.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine_quality_prediction.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.wine_quality_prediction.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.wine_quality_prediction.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -29,6 +30,16 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.initiate_data_transformation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.initiate_model_trainer()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=====x")
 except Exception as e:
     logger.exception(e)
--- src/wine_quality_prediction/entity/config_entity.py
+++ src/wine_quality_prediction/entity/config_entity.py
@@ -16,9 +16,20 @@ class DataValidationConfig:
     all_schema: dict ## have all schema details in schema.yaml
 
 ## Data transformation entity
-
 @dataclass
 class DataTransformationConfig:
     root_dir : Path
     data_path: Path
+
+## Model trainer Config
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha : float ##Should be in params.yaml file
+    l1_ratio: float ##Should be in params.yaml file
+    target_column: str
+
     
